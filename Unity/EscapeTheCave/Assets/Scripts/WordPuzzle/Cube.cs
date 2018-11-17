eb0beae1a2baf38ade6ec785aaef653a2056c218
Kirill <kkcorp2008@gmail.com>
2018-11-17 18:52:30 +0100

    fix bugs

--- Unity/EscapeTheCave/Assets/Scripts/WordPuzzle/Cube.cs
+++ Unity/EscapeTheCave/Assets/Scripts/WordPuzzle/Cube.cs
@@ -11,12 +11,18 @@ public class Cube : MonoBehaviour {
     private Vector3 prefabPosition;
     private float prefabRotation;
     private Vector3 originPosition;
+    private Vector3 originLocalPosition;
     private static Vector3 size = new Vector3(-1, -1, -1);
     private float currentEmissionLevel = 0.0f;
     private bool pressed = false;
     private float currentPressedLevel = 0.0f;
+    private bool ready = false;
+    private bool moving = false;
+    private Wall wallInstance;
+    public bool correctKey;
+    public bool signalSent = false;
 
-    public void setup(GameObject init, char letter, int row, int column)
+    public void setup(GameObject init, Wall wall, bool correctKey, char letter, int row, int column)
     {
         prefabPosition = init.GetComponent<Transform>().position;
         prefabRotation = init.GetComponent<Transform>().eulerAngles.y;
@@ -26,11 +32,14 @@ public class Cube : MonoBehaviour {
         GetComponent<Renderer>().material.SetTextureOffset("_DetailAlbedoMap", new Vector2(Random.Range(0f, 1f), Random.Range(0f, 1f)));
 
         this.letter = letter;
+    
+        setOriginPosition(row, column);
 
-        if (!(column == 0 && row == 0))
-        {
-            setOriginPosition(row, column);
-        }
+        wallInstance = wall;
+
+        this.correctKey = correctKey;
+
+        ready = true;
     }
 
     void Start () {
@@ -38,25 +47,58 @@ public class Cube : MonoBehaviour {
     }
 	
 	void Update () {
-        highlight();
-        move();
+        if (ready)
+        {
+            if (!moving && pressed && !signalSent)
+            {
+                Debug.Log(correctKey);
+                wallInstance.pressedKey(correctKey);
+                signalSent = true;
+            }
+            highlight();
+            move();
+        }
+    }
+
+    public void finish()
+    {
+        ready = false;
+        setEmmission(0.0f);
+    }
+
+    public void unpress()
+    {
+        if (' ' != letter)
+        {
+            pressed = false;
+            moving = true;
+            signalSent = false;
+        }
     }
 
     public void press()
     {
-        if (' ' != letter) pressed = true;
+        if (' ' != letter)
+        {
+            pressed = true;
+            moving = true;
+        }
     } 
 
     void move()
     {
+        moving = false;
         if (pressed && 0.03f > currentPressedLevel)
         {
+            moving = true;
             currentPressedLevel += Time.deltaTime * 0.2f;
+            setLocalPosition(originLocalPosition + new Vector3(0.0f, 0.0f, currentPressedLevel));
         } else if (!pressed && 0.0f < currentPressedLevel)
         {
+            moving = true;
             currentPressedLevel -= Time.deltaTime * 0.2f;
+            setLocalPosition(originLocalPosition + new Vector3(0.0f, 0.0f, currentPressedLevel));
         }
-        GetComponent<Transform>().position = originPosition + new Vector3(0.0f, 0.0f, -currentPressedLevel);
     }
 
     void highlight()
@@ -65,24 +107,37 @@ public class Cube : MonoBehaviour {
         if (focused && letter != ' ' && 0.1f > currentEmissionLevel)
         {
             currentEmissionLevel += Time.deltaTime * 0.4f;
+            setEmmission(currentEmissionLevel);
         } else if (!focused && 0.0f < currentEmissionLevel)
         {
             currentEmissionLevel -= Time.deltaTime * 0.7f;
+            setEmmission(currentEmissionLevel);
         }
-        GetComponent<Renderer>().material.SetColor("_EmissionColor", Color.yellow * Mathf.LinearToGammaSpace(currentEmissionLevel));
+        
     }
 
     void setOriginPosition(int row, int column)
     {
         // ATTENTION! maybe x or z axe may change
-        Vector3 offset = Quaternion.AngleAxis(prefabRotation + 90.0f, new Vector3(0, 1, 0)) * new Vector3(0, (size.y + deltaHeight) * -row, (size.z + deltaWidth) * -column);
+        Vector3 offset = Quaternion.AngleAxis(prefabRotation + 90.0f, new Vector3(0, 1, 0)) * new Vector3((Random.Range(0f, 100f) -50.0f) / 6000.0f, (size.y + deltaHeight) * -row, (size.z + deltaWidth) * -column);
         originPosition = prefabPosition + offset;
         setPosition(originPosition);
+        originLocalPosition = GetComponent<Transform>().localPosition;
+    }
+
+    void setEmmission(float level)
+    {
+        GetComponent<Renderer>().material.SetColor("_EmissionColor", Color.yellow * Mathf.LinearToGammaSpace(level));
     }
 
     void setPosition(Vector3 target)
     {
         GetComponent<Transform>().position = target;
+    }
+
+    void setLocalPosition(Vector3 target)
+    {
+        GetComponent<Transform>().localPosition = target;
     }
 
     public static Texture2D loadTextFor(char letter)
--- Unity/EscapeTheCave/Assets/Scripts/WordPuzzle/Wall.cs
+++ Unity/EscapeTheCave/Assets/Scripts/WordPuzzle/Wall.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,6 +9,8 @@ public class Wall : MonoBehaviour {
     public GameObject WallController;
     public string text = "Test es testet Test testet set test teste";
     public int lineLength = 5;
+    public int[] rightKeys = {1, 5};
+    public int correctCount = 0;
     static List<GameObject> list = new List<GameObject>();
 
 
@@ -15,22 +18,60 @@ public class Wall : MonoBehaviour {
         list.Add(firstBrick);
 
         for (int index = 1; index < text.Length; index++)
-        {
+        {           
             GameObject newCube = Instantiate(firstBrick, WallController.GetComponent<Transform>());
             list.Add(newCube);
-            newCube.GetComponent<Cube>().setup(firstBrick, text[index], (int)index / lineLength, index % lineLength);   
+            newCube.GetComponent<Cube>().setup(firstBrick, this, indexCorrect(index + 1), text[index], (int)index / lineLength, index % lineLength);   
         }
-        firstBrick.GetComponent<Cube>().setup(firstBrick, text[0], 0, 0);
+        firstBrick.GetComponent<Cube>().setup(firstBrick, this, indexCorrect(1), text[0], 0, 0);
     }
 
     private void Update()
     {
-        // focusing a button
         if (null != Interact.focused && Interact.pressedE && "pressable" == Interact.focused.tag && ("WordPuzzleButton" == Interact.focused.name || "WordPuzzleButton(Clone)" == Interact.focused.name))
         {
             Interact.focused.GetComponent<Cube>().press();
         }
+        finished();
     }
 
+    private bool finished()
+    {
+        if (correctCount == rightKeys.Length)
+        {
+            foreach (GameObject cube in list)
+            {
+                cube.GetComponent<Cube>().finish();
+            }
+            Debug.Log("WordPuzzleSolved");
+            return true;
+        }
+        return false;
+
+    }
+
+    public void pressedKey(bool correctKey)
+    {
+        if (correctKey)
+        {
+            correctCount++;
+        } else
+        {
+            correctCount = 0;
+            foreach (GameObject cube in list)
+            {
+                cube.GetComponent<Cube>().unpress();
+            }
+        }
+    }
+
+    bool indexCorrect(int index)
+    {
+        if (-1 != Array.IndexOf(rightKeys, index))
+        {
+            return true;
+        }
+        return false;
+    }
     
 }
