f38a63d1ee069655fb09ce34d085071233dd7c84
Ruben Aegerter <ruben.aegerter@gmx.de>
2018-11-18 19:34:14 +0100

    Save

---
+++ Unity/EscapeTheCave/Assets/Scripts/Gate/DoorController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour {
+
+    [SerializeField] bool isInteracted;
+
+	// Use this for initialization
+	void Start () {
+        isInteracted = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(isInteracted)
+        {
+            transform.RotateAround(transform.position - new Vector3(3, 0, 0), transform.up, -45 * Time.deltaTime);
+        }
+	}
+}
