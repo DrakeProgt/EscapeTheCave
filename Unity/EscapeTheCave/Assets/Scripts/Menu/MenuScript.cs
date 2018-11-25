77a6a9ae295a725b3908eda2b560c76ae35c23de
MyNameIsNico <nico.pietzsch@web.de>
2018-11-25 18:33:03 +0100

    options

--- Unity/EscapeTheCave/Assets/Scripts/Menu/MenuScript.cs
+++ Unity/EscapeTheCave/Assets/Scripts/Menu/MenuScript.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuScript : MonoBehaviour
 {
@@ -10,16 +11,33 @@ public class MenuScript : MonoBehaviour
     public GameObject mainPanel;
     public GameObject soundPanel;
     public GameObject controlsPanel;
+    public GameObject B;
 
     public GameObject eventSystem;
     private bool gamepadConected;
+    private GameObject lastButtonPressed;
 
     // Use this for initialization
     private void Start ()
     {
+        lastButtonPressed = null;
         gamepadConected = Input.GetJoystickNames().Length > 0;
         if (gamepadConected)
             eventSystem.GetComponent<UnityEngine.EventSystems.EventSystem>().SetSelectedGameObject(GameObject.Find("Start"));
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+            if (gamepadConected && lastButtonPressed != null)
+            {
+                eventSystem.GetComponent<UnityEngine.EventSystems.EventSystem>().SetSelectedGameObject(lastButtonPressed);
+                B.SetActive(false);
+                if (lastButtonPressed.name == "VolumeButton")
+                    lastButtonPressed = GameObject.Find("Sound");
+                else
+                    lastButtonPressed = null;
+            }
     }
 
     public void ExitGame()
@@ -50,7 +68,18 @@ public class MenuScript : MonoBehaviour
         mainPanel.SetActive(false);
 
         if (gamepadConected)
+        {
             eventSystem.GetComponent<UnityEngine.EventSystems.EventSystem>().SetSelectedGameObject(GameObject.Find("Sound"));
+
+        }
+
+        //GameObject.Find("TextSound").GetComponent<Text>().color = new Color(0, 1, 1, 1);
+        //GameObject.Find("TextControls").GetComponent<Text>().color = new Color(1, 1, 1, 1);
+
+        if (gamepadConected)
+            eventSystem.GetComponent<UnityEngine.EventSystems.EventSystem>().SetSelectedGameObject(GameObject.Find("MusicButton"));
+
+        lastButtonPressed = GameObject.Find("Sound");
     }
 
     public void ShowControlsOptions()
@@ -61,6 +90,11 @@ public class MenuScript : MonoBehaviour
 
         if (gamepadConected)
             eventSystem.GetComponent<UnityEngine.EventSystems.EventSystem>().SetSelectedGameObject(GameObject.Find("Controls"));
+
+        //GameObject.Find("TextSound").GetComponent<Text>().color = new Color(1, 1, 1, 1);
+        //GameObject.Find("TextControls").GetComponent<Text>().color = new Color(0, 1, 1, 1);
+
+        lastButtonPressed = GameObject.Find("Controls");
     }
 
     public void ShowCredits()
@@ -82,6 +116,8 @@ public class MenuScript : MonoBehaviour
         mainPanel.SetActive(true);
         optionsPanel.SetActive(false);
         creditsPanel.SetActive(false);
+        controlsPanel.SetActive(false);
+        soundPanel.SetActive(false);
         if (gamepadConected)
             eventSystem.GetComponent<UnityEngine.EventSystems.EventSystem>().SetSelectedGameObject(GameObject.Find("Start"));
 
@@ -94,13 +130,42 @@ public class MenuScript : MonoBehaviour
         SceneManager.LoadScene("NicoSampleScene");
     }
 
+    public void EditMusic()
+    {
+        float value = GameObject.Find("SliderMusic").GetComponent<Slider>().value;
+        if (value == 0)
+            value = 1;
+        else
+            value = 0;
+        GameObject.Find("SliderMusic").GetComponent<Slider>().value = value;
+    }
+
+    public void EditVolume()
+    {
+        if (gamepadConected)
+            eventSystem.GetComponent<UnityEngine.EventSystems.EventSystem>().SetSelectedGameObject(GameObject.Find("SliderVolume"));
+        
+        lastButtonPressed = GameObject.Find("VolumeButton");
+        if (gamepadConected)
+            B.SetActive(true);
+    }
+
     public void SetMusicEnabled(float value)
     {
+        GameObject sliderHandle = GameObject.Find("HandleMusic");
+        if (sliderHandle == null)
+            return;
 
+        if (value == 0)
+            sliderHandle.GetComponent<Image>().color = new Color(1, 1, 1, 1);
+        else
+            sliderHandle.GetComponent<Image>().color = new Color(0, 1, 1, 1);
+
+        //TODO: set music in GameManager
     }
 
     public void SetMusicVolume(float value)
     {
-
+        //TODO: set volume in GameManager
     }
 }
