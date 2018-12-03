b90ac2e5b9d1dd1f5f9e2d3a3c899a4fa5355a90
Kirill <kkcorp2008@gmail.com>
2018-12-03 17:45:56 +0100

    sort objects and add diary

---
+++ Unity/EscapeTheCave/Assets/Scripts/Diary/DiaryHandler.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiaryHandler : MonoBehaviour {
+
+Animator m_Animator;
+private bool opened;
+
+    // Use this for initialization
+    void Start () {
+
+		m_Animator = GetComponent<Animator>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if(Input.GetKeyDown(KeyCode.O)){
+			opened = m_Animator.GetBool("DiaryOpened");
+			if(opened){
+				opened = false;
+			}else{
+				opened = true;
+			}
+			m_Animator.SetBool("DiaryOpened",opened);
+		}
+
+
+	}
+}
